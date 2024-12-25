0f6f8a60b0dcf40eefb794583087f18fd11b5117
najims2016 <naji58809@gmail.com>
2024-12-25 12:59:57 +0300

    p

--- resources/views/site/page/show.blade.php
+++ resources/views/site/page/show.blade.php
@@ -1,37 +1,33 @@
 @extends('site.layouts.home.layout')
 @section('content')
-<div class="container-fluid mt-3 pt-3">
-    <div class="row">
-        <!-- القسم الجانبي -->
-        @include('site.content.profile-sidebar')
+    <div class="container-fluid mt-3 pt-3">
+        <div class="row text-center" style="justify-content: center;">
+            <!-- القسم الجانبي -->
 
-       
-        <section class=" content-sec col-lg-7 col-md-6">
 
-            <div style="margin-top: 120px"></div>
-            <div class="bg-white p-4 rounded shadow box-form">
-                 
-                <div class="edit-details__content">
-                  <div class="category-details">
-                    <h3>{{ $page['tr_title'] }}</h3>
-                    <p>{{Str::of($page['tr_content'])->toHtmlString()}}</p>
-                  </div>
-                </div>
+            <section class=" content-sec col-lg-7 col-md-6   ">
+
+                <div style="margin-top: 120px"></div>
+                <div class="bg-white p-4 rounded shadow box-form">
+
+                    <div class="edit-details__content">
+                        <div class="category-details">
+                            <h3>{{ $page['tr_title'] }}</h3>
+                            <p>{{ Str::of($page['tr_content'])->toHtmlString() }}</p>
                         </div>
+                    </div>
+                </div>
 
 
-        </section>
+            </section>
+        </div>
     </div>
-</div>
-
-
 @endsection
 @section('js')
-<script>
- 
-    var success_msg = "تم الحفظ بنجاح";
-    var fail_msg = "لم يتم الحفظ";
-</script>
-<script src="{{ url('assets/site/js/sweetalert.min.js') }}"></script>
-<script src="{{ url('assets/site/js/custom/setting.js') }}"></script>
+    <script>
+        var success_msg = "تم الحفظ بنجاح";
+        var fail_msg = "لم يتم الحفظ";
+    </script>
+    <script src="{{ url('assets/site/js/sweetalert.min.js') }}"></script>
+    <script src="{{ url('assets/site/js/custom/setting.js') }}"></script>
 @endsection
